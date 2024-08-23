c7cff9863885c0c3afb321b49e0db4dc149f59ed
Mikhail Shafigullin <mikhailshafigullin@192.168.0.102>
2024-08-23 12:52:03 +0200

    add code to understand how to write on sharp

--- src/Level1.cs
+++ src/Level1.cs
@@ -3,13 +3,25 @@ using System;
 
 public partial class Level1 : Node2D
 {
+	private ColorRect nonameHuisBlock;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		nonameHuisBlock = GetNode<ColorRect>("%SquareUI");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GD.Print("whatever " + nonameHuisBlock.Name);
 	}
+	
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event is InputEventKey eventKey)
+			if (eventKey.Pressed && eventKey.Keycode == Key.Escape)
+				nonameHuisBlock.Visible = !nonameHuisBlock.Visible;
+	}
+	
 }
