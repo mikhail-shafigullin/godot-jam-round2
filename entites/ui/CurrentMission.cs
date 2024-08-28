4f0212a0a6e4473dd081d64c4c1af8cbeb82b960
AltTabber <mikeice414@gmail.com>
2024-08-28 11:57:18 +0200

    remove comments from merge conflict

--- entites/ui/CurrentMission.cs
+++ entites/ui/CurrentMission.cs
@@ -60,10 +60,4 @@ public partial class CurrentMission : Control
 			
 		}
 	}
-	
-	/*
-	 * * branch main -> FETCH_HEAD = [up to date] main -> origin/main hint:
-	 * You have divergent branches and need to specify how to reconcile them.
-	 * hint: You can do so by running one of the following commands sometime before hint: your next pull: hint: hint: git config pull.rebase false # merge hint: git config pull.rebase true # rebase hint: git config pull.ff only # fast-forward only hint: hint: You can replace "git config" with "git config --global" to set a default hint: preference for all repositories. You can also pass --rebase, --no-rebase, hint: or --ff-only on the command line to override the configured default per hint: invocation. Need to specify how to reconcile divergent branches.
-	 */
 }
